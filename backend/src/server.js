907911de8c29a9753c9e2691debc65450fecb9e5
Yashwant Singh <yashwantsingh@yashwants-MacBook-Air.local>
2026-03-16 17:31:13 +0530

    changes made fro deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRouters from "./Routes/notesRouters.js";
 import connectDB from "./config/db.js";
@@ -10,16 +11,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV === "!production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouters);
+
+app.use(express.static(path.join(__dirname, "../frontend/notesProject/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "notesProject", "dist", "index.html"),
+    );
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
